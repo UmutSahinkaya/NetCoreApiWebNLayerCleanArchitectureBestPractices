d155c318d5dbabada2d2834ef419428363846083
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-09-06 09:33:19 +0300

    Product Repository

--- Repositories/GenericRepository.cs
+++ Repositories/GenericRepository.cs
@@ -3,12 +3,13 @@ using System.Linq.Expressions;
 
 namespace App.Repositories;
 
-public class GenericRepository<T>(AppDbContext context):IGenericRepository<T> where T : class
+public class GenericRepository<T>(AppDbContext context) : IGenericRepository<T> where T : class
 {
-    private readonly DbSet<T> _dbSet=context.Set<T>();
+    protected AppDbContext Context = context;
+    private readonly DbSet<T> _dbSet = context.Set<T>();
 
     public IQueryable<T> GetAll() => _dbSet.AsQueryable().AsNoTracking();
-    public IQueryable<T> Where(Expression<Func<T, bool>> predicate)=> _dbSet.Where(predicate).AsNoTracking();
+    public IQueryable<T> Where(Expression<Func<T, bool>> predicate) => _dbSet.Where(predicate).AsNoTracking();
     public ValueTask<T?> GetByIdAsync(int id) => _dbSet.FindAsync(id);
     public async ValueTask AddAsync(T entity) => await _dbSet.AddAsync(entity);
     public void Update(T entity) => _dbSet.Update(entity);
