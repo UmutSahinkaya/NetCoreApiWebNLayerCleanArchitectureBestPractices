43846dfe0590fdf661f831c6feb0bd2e17918791
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-09-06 09:42:53 +0300

    Product Repository DI Contanier

--- Repositories/Extensions/RepositoryExtensions.cs
+++ Repositories/Extensions/RepositoryExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using App.Repositories.Products;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,6 +17,8 @@ public static class RepositoryExtensions
                 sqlServerOptionsAction.MigrationsAssembly(typeof(RepositoryAssembly).Assembly.FullName);
             });
         });
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
         return services;
     }
 }
