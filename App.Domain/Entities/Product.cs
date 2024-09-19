274430435f5c513ff0248cf86966c38e718cd86c
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-09-19 14:42:46 +0300

    Domains

---
+++ App.Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using App.Domain.Entities.Common;
+
+namespace App.Domain.Entities;
+
+public class Product:BaseEntity<int>,IAuditEntity
+{
+    //public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = default!; // Burası null olamaz.
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
+}
